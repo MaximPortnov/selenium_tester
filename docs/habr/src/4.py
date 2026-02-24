6ca659b36101bad4f5c198a3a0c04ae7aa1853fb
MaximPortnov <portmaxalex@mail.ru>
2026-02-24 15:45:44 +0700

    fix

--- docs/habr/src/4.py
+++ docs/habr/src/4.py
@@ -60,7 +60,6 @@ def click_in_any_frame(
                 "arguments[0].scrollIntoView({block: 'center'});", element
             )
             driver.execute_script("arguments[0].click();", element)
-            driver.switch_to.default_content()
             return
         time.sleep(0.2)
     raise TimeoutException(f"Element not found for click: {by}={value}")
